afd9bbb6fbd16b636f8f39a7cf44d620803f28b1
Yael Brown <yaelrbrown@gmail.com>
2021-04-02 20:28:25 -0500

    added aliases

---
+++ yael_aliases.sh
@@ -0,0 +1,6 @@
+echo "alias m=minikube" > ~/.bash_profile
+echo "alias k=kubernetes" > ~/.bash_profile
+echo "alias d=docker" > ~/.bash_profile
+echo "alias dc=docker-compose" > ~/.bash_profile
+
+source ~/.bash_profile
