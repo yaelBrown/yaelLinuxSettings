5e67090e29a554bb8d63474b59721f9b2a96ae34
yaelBrown <yaelrbrown@gmail.com>
2023-06-16 08:33:07 -0500

    feat: update linux commands

--- yael_aliases.sh
+++
@@ -1,6 +0,0 @@
-echo "alias m=minikube" > ~/.bash_profile
-echo "alias k=kubernetes" > ~/.bash_profile
-echo "alias d=docker" > ~/.bash_profile
-echo "alias dc=docker-compose" > ~/.bash_profile
-
-source ~/.bash_profile
---
+++ yael_aliases_linux.sh
@@ -0,0 +1,12 @@
+echo alias m=minikube >> ~/.bash_profile
+echo alias k=kubernetes >> ~/.bash_profile
+echo alias d=docker >> ~/.bash_profile
+echo alias dc=docker-compose >> ~/.bash_profile
+echo "alias ls='ls -la'" >> ~/.bash_profile
+echo "alias ..='cd ..'" >> ~/.bash_profile
+echo "alias .='open .'" >> ~/.bash_profile
+echo "alias gs='git status'" >> ~/.bash_profile
+echo "alias ga='git add .'" >> ~/.bash_profile
+echo "alias gp='git push'" >> ~/.bash_profile
+echo "alias x=exit" >> ~/.bash_profile
+
